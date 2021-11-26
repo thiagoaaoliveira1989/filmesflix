71fd37cf27087516e25cdd25048e1666ad2e116a
Thiago Araujo de Aguiar Oliveira <45134114+thiagoaaoliveira1989@users.noreply.github.com>
2021-11-26 05:17:04 -0300

    atualzacao

--- index.php
+++ index.php
@@ -6,15 +6,15 @@ require_once __DIR__ . "/vendor/autoload.php";
 $rota = $_SERVER["REQUEST_URI"];
 $metodo = $_SERVER["REQUEST_METHOD"];
 
-require_once __DIR__ ."/classes/repository/FilmesRepositoryPDO.php";
+require_once __DIR__ ."/app/classes/repository/FilmesRepositoryPDO.php";
 
 if ($rota === "/"){
-    require_once __DIR__ ."/classes/view/galeria.php";
+    require_once __DIR__ ."/app/classes/view/galeria.php";
     exit();
 }
 
 if ($rota === "/novo"){
-    if($metodo == "GET") require_once __DIR__ . "/classes/view/cadastrar.php";
+    if($metodo == "GET") require_once __DIR__ . "/app/classes/view/cadastrar.php";
     if($metodo == "POST") {
         $controller = new FilmesController();
         $controller->save($_REQUEST);
@@ -29,7 +29,7 @@ if (substr($rota, 0, strlen("/favoritar")) === "/favoritar") {
 }
 
 if (substr($rota, 0, strlen("/filmes")) === "/filmes") {
-    if($metodo == "GET") require_once __DIR__ ; "/classes/view/galeria.php";
+    if($metodo == "GET") require_once __DIR__ ; "/app/classes/view/galeria.php";
     if($metodo == "DELETE"){
         $controller = new FilmesController();
         $controller->delete(basename($rota));
