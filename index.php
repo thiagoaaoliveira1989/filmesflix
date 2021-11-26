98c32a5ac5bf945486d6c35f32f99613f40601ed
Thiago Araujo de Aguiar Oliveira <45134114+thiagoaaoliveira1989@users.noreply.github.com>
2021-11-26 16:02:03 -0300

    configuraçao original

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
-
-require_once __DIR__ . "/vendor/autoload.php";
-require_once __DIR__ ."/classes/repository/FilmesRepositoryPDO.php";
+require_once __DIR__ ."/repository/FilmesRepositoryPDO.php";
 
 $rota = $_SERVER["REQUEST_URI"];
 $metodo = $_SERVER["REQUEST_METHOD"];
@@ -9,12 +16,12 @@ $metodo = $_SERVER["REQUEST_METHOD"];
 
 
 if ($rota === "/"){
-    require_once __DIR__ ."/classes/view/galeria.php";
+    require_once __DIR__ ."/view/galeria.php";
     exit();
 }
 
 if ($rota === "/novo"){
-    if($metodo == "GET") require_once __DIR__ . "/classes/view/cadastrar.php";
+    if($metodo == "GET") require_once __DIR__ . "/view/cadastrar.php";
     if($metodo == "POST") {
         $controller = new FilmesController();
         $controller->save($_REQUEST);
@@ -29,7 +36,7 @@ if (substr($rota, 0, strlen("/favoritar")) === "/favoritar") {
 }
 
 if (substr($rota, 0, strlen("/filmes")) === "/filmes") {
-    if($metodo == "GET") require_once __DIR__ ; "/classes/view/galeria.php";
+    if($metodo == "GET") require_once __DIR__ ; "/view/galeria.php";
     if($metodo == "DELETE"){
         $controller = new FilmesController();
         $controller->delete(basename($rota));
@@ -37,4 +44,7 @@ if (substr($rota, 0, strlen("/filmes")) === "/filmes") {
     exit();
 }
 
-require_once __DIR__ . "/classes/view/404.php";
+require_once __DIR__ . "/view/404.php";
+?>
+</body>
+</html>
